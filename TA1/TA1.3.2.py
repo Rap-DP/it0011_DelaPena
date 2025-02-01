ac024ca24653758d3212ba3df896834c8569f247
Raphael Dela Peña <rapdelapena28@gmail.com>
2025-02-01 12:33:35 +0800

    TA1.3.2

---
+++ TA1/TA1.3.2.py
@@ -0,0 +1,12 @@
+i = 1
+while i <= 5:
+    j = 5
+    while j > i:
+        print(end="")
+        j -= 1
+    k = 1
+    while k <= i * 2 - 1:  
+        print(i * 2 - 1, end="")
+        k += 1
+    print("\n")  
+    i += 1
