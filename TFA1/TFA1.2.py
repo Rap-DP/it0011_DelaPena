a67fc618e296656c246ce3d0ed0af737ea8f880d
Rap-DP <rapdelapena28@gmail.com>
2025-02-03 09:42:31 +0800

    TFA1.2

--- TFA1/TFA1.2.py
+++ TFA1/TFA1.2.py
@@ -1,5 +1,11 @@
 f_name = input("Enter your first name: ")
 l_name = input("Enter your last name: ")
+full_name = f_name + " " + l_name
+up_full_name = f_name.upper() + " " + l_name.upper()
+low_full_name = f_name.lower() + " " + l_name.lower()
+length = len(full_name)
+print("Full Name:", full_name)
+print("Full Name (Upper Case):", up_full_name)
+print("Full Name (Lower Case):", low_full_name)
+print("Length of Full Name:", length)
 
-
-print("Full Name:", f_name+" "+l_name)
