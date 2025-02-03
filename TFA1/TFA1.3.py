f9d2e68d9e002ec193060fa27699d6a3a591e45e
Rap-DP <rapdelapena28@gmail.com>
2025-02-03 10:10:51 +0800

    TFA1.3

--- TFA1/TFA1.3.py
+++ TFA1/TFA1.3.py
@@ -4,7 +4,7 @@ age = input("Enter your age: ")
 contact = input("Enter your contact number: ")
 course = input("Enter your course: ")
 
-f = open("C:\\Users\\202310437\\Documents\\GitHub\\it0011_DelaPena\\TFA1\\students.txt", "a")
+f = open("TFA1\\students.txt", "a")
 f.write("Last Name: " + l_name +"\n")
 f.write("First Name: " + f_name +"\n")
 f.write("Age: "+ age +"\n")
